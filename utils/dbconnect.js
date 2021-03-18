13597d3d876f1b1d10e62cd218888cc324ffb65f
SaidShatila <57522137+SaidShatila@users.noreply.github.com>
2021-03-18 14:01:10 +0200

    heroku deploy

--- utils/dbconnect.js
+++ utils/dbconnect.js
@@ -2,18 +2,18 @@ const mysql = require('mysql');
 
 //Connect to MYSQL
 var con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'aggrigator'
+    host: 'sql11.freemysqlhosting.net',
+    user: 'sql11399822',
+    password: 'w3Gg9qqiGd',
+    database: 'sql11399822'
 
 }
 );
 
-con.connect((err)=>{
-    if(err){
+con.connect((err) => {
+    if (err) {
         console.log('Error connecting to DB');
-    }else{
+    } else {
         console.log('connection successful');
     }
 })
